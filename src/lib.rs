c6d85c309a66fe8d048bcbf30cbf580254bc5a41
ashWhiteHat <phantomofrotten@gmail.com>
2024-10-01 21:33:50 +0900

    pairing: final exp and signed pairing test

--- src/lib.rs
+++ src/lib.rs
@@ -23,11 +23,29 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_final_exp() {
+        assert_eq!(Fq12::one().final_exp(), Gt::identity());
+    }
+
+    #[test]
     fn test_generator_pairing() {
         let g1 = G1Affine::generator();
         let g2 = G2Affine::generator();
         let gt = Gt::generator();
 
         assert_eq!(gt, AteParing::pairing(g1, g2));
+    }
+
+    #[test]
+    fn test_signed_pairing() {
+        let g = G1Affine::generator();
+        let h = G2Affine::generator();
+
+        let p = -AteParing::pairing(g, h);
+        let q = AteParing::pairing(g, -h);
+        let r = AteParing::pairing(-g, h);
+
+        assert_eq!(p, q);
+        assert_eq!(q, r);
     }
 }
